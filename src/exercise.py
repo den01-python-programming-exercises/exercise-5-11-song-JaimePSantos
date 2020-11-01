fb38b513f1f26f6f4d4d8b2c4d87a282fbd8b13f
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-11-01 01:49:59 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
+from song import Song
 def main():
     #write your code below this line
+  jack_sparrow = Song("The Lonely Island", "Jack Sparrow", 196)
+  another_sparrow = Song("The Lonely Island", "Jack Sparrow", 196)
 
+  if (jack_sparrow == another_sparrow):
+      print("Songs are equal.")
+
+  if (jack_sparrow == "Another object"):
+      print("Strange things are afoot.")
 if __name__ == '__main__':
-    main()
+  main()
+  from song import Song
+else:
+  from src.song import Song
+
--- src/song.py
+++ src/song.py
@@ -4,3 +4,13 @@ class Song:
         self.name = name
         self.artist = artist
         self.duration = duration
+
+    def __eq__(self,other):
+
+      if not isinstance(other,Song):
+        return False
+      
+      if(self.name==other.name and self.artist == other.artist and self.duration == other.duration):
+        return True
+      
+      return False
